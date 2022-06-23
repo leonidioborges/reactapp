67580ba390d52b1578c1eaad0b6625d40432e1d3
Leonidio Borgesm <leonidio.borges@gmail.com>
2022-06-22 23:16:05 -0300

    Aulas Discover - Fundamentos React

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+//import App from './App'
+import {Home} from './pages/Home'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export function Home() {
+
+    return (
+        <div className='container'>
+            <h1>Lista de Presença</h1>
+            <input type="Text" placeholder="Digite o nome ...." />
+            <button type="button">Adicionar</button>
+        </div>
+    ) 
+}
